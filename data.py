f6ba9022be8da593b5d90dbbd6cc731531386f26
Diego Emilio Enriquez Nares <134454409+diegotita4@users.noreply.github.com>
2024-10-14 19:15:28 -0600

    Initial commit

---
+++ data.py
@@ -0,0 +1,12 @@
+
+"""
+# -- --------------------------------------------------------------------------------------------------- -- #
+# -- project: A SHORT DESCRIPTION OF THE PROJECT                                                         -- #
+# -- script: data.py : python script for data collection                                                 -- #
+# -- author: YOUR GITHUB USER NAME                                                                       -- #
+# -- license: THE LICENSE TYPE AS STATED IN THE REPOSITORY                                               -- #
+# -- repository: YOUR REPOSITORY URL                                                                     -- #
+# -- --------------------------------------------------------------------------------------------------- -- #
+"""
+
+dict_test = {'key_a': 'a', 'key_b': 'b'}
---
+++ functions.py
@@ -0,0 +1,10 @@
+
+"""
+# -- --------------------------------------------------------------------------------------------------- -- #
+# -- project: A SHORT DESCRIPTION OF THE PROJECT                                                         -- #
+# -- script: functions.py : python script with general functions                                         -- #
+# -- author: YOUR GITHUB USER NAME                                                                       -- #
+# -- license: THE LICENSE TYPE AS STATED IN THE REPOSITORY                                               -- #
+# -- repository: YOUR REPOSITORY URL                                                                     -- #
+# -- --------------------------------------------------------------------------------------------------- -- #
+"""
